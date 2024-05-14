66a2273b249842bc061228a94de46c200b009001
Ekundayo Emmanuel <48570185+Mannyemmy@users.noreply.github.com>
2024-05-14 07:07:12 +0100

    Add files via upload

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -1,36 +1,15 @@
-import {
-  SiGithub,
-  SiInstagram,
-  SiLinkedin,
-  SiNpm,
-  SiTwitter,
-} from '@icons-pack/react-simple-icons';
+import { SiGithub, SiGmail } from '@icons-pack/react-simple-icons';
 import { CMSLink } from 'edit-me/types/CMSLink';
 
 export const links: CMSLink[] = [
   {
-    href: 'https://github.com',
+    href: 'https://github.com/mannyemmy',
     icon: SiGithub,
     title: 'GitHub',
   },
   {
-    href: 'https://www.instagram.com',
-    icon: SiInstagram,
-    title: 'Instagram',
-  },
-  {
-    href: 'https://www.linkedin.com',
-    icon: SiLinkedin,
-    title: 'LinkedIn',
-  },
-  {
-    href: 'https://www.npmjs.com',
-    icon: SiNpm,
-    title: 'NPM',
-  },
-  {
-    href: 'https://twitter.com',
-    icon: SiTwitter,
-    title: 'Twitter',
+    href: 'mannyedk@gmail.com',
+    icon: SiGmail,
+    title: 'Mail',
   },
 ];
